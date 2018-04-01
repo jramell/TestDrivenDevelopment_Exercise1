8b34fe188ed101e52fe4f7baed75de63c059e70f
jramell <sergiomaderarangel@gmail.com>
2018-03-31 21:47:52 -0500

    Iteration 1, step 3 - Added test for element count when string has 2 numbers (Red)

--- test_arrayStatistics.py
+++ test_arrayStatistics.py
@@ -7,3 +7,6 @@ class TestArrayStatistics(TestCase):
 
     def test_numElems_oneElem(self):
         self.assertEqual(ArrayStatistics().numElems("1"), 1)
+
+    def test_numElems_twoElems(self):
+        self.assertEqual(ArrayStatistics().numElems("1,2"), 2)
