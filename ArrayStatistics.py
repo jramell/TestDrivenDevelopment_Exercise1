4adbc43c72315637d13edc98833bf6dcaac2b97c
jramell <sergiomaderarangel@gmail.com>
2018-03-31 21:58:35 -0500

    Iteration 2, step 1 - Added test for min and element count when string is empty (Red)

--- ArrayStatistics.py
+++ ArrayStatistics.py
@@ -4,3 +4,6 @@ class ArrayStatistics:
             return 0;
 
         return len(stringWithCommaSeparatedNumbers.split(","));
+
+    def numElemsIter2(self, stringWithCommaSeparatedNumbers):
+        return 1;
--- test_arrayStatistics.py
+++ test_arrayStatistics.py
@@ -13,3 +13,6 @@ class TestArrayStatistics(TestCase):
 
     def test_numElems_nElems(self):
         self.assertEqual(ArrayStatistics().numElems("1,2,3,4,10,14"), 6)
+
+    def test_numElemsIter2_empty(self):
+        self.assertEqual(ArrayStatistics().numElemsIter2(""), [0, -1])
